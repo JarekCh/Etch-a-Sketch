6f8d3c0ca678c3b3bb6534124f6e529c7a3b1108
Jaroslaw Charchula <charchula@gmail.com>
2022-03-24 17:25:39 +0100

    add - css - final file - js - final logic - README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Etch-a-Sketch
+
+Sketch an exercise during The odin Project course.
+Simple app that generates grid and allow user to change background color on grid cell.
+
+## Technologies
+- JS (dom manipulation)
+- CSS 
+- HTML
+
+## Usage
+Click link: https://jarekch.github.io/Etch-a-Sketch/
---
+++ js/index.js
@@ -0,0 +1,45 @@
+// index.js
+
+let gridContainer = document.querySelector(".grid");
+let size = parseInt(window.getComputedStyle(gridContainer).width, 10);
+
+// Function that assign color on to boxes inside grid
+function changeGridColor(e) {
+   let color = `rgb(${[0, 0, 0]})`
+    Object.assign(e.target.style, {
+     backgroundColor : color    
+   });
+}
+
+// Function that remove old grid and allow to get number between 16 to 100 to make new one
+function ResetButton(e) {
+  let gridNumber = +prompt("Enter the grid size you want:");
+  if (gridNumber > 100 || gridNumber < 16)
+    window.alert("Pick number beetwen 16 ann 100");
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.lastChild);
+  }
+  createGrid(gridNumber);
+}
+
+window.onload = function() {
+    createGrid(24);
+};
+
+// Function that creates new grid 
+function createGrid(gridSize) {
+    for (let rowCol = 0; rowCol < gridSize ** 2; rowCol++) {
+      let gridCell = document.createElement("div");
+      Object.assign(gridCell.style, {
+        height : `${(size / gridSize) - 2}px`,
+        width : `${(size / gridSize) - 2}px`
+      });
+      gridCell.classList.add("gridBox");
+      gridCell.addEventListener('mouseover', changeGridColor)
+      gridContainer.appendChild(gridCell);      
+    }
+}
+
+const resetButton = document.querySelector(".resetButton");
+resetButton.addEventListener('click', ResetButton);
+

10e28b6ea4602da5588e2b7b74de9642574946b9
JarekCh <Charchula@gmail.com>
2022-03-24 19:12:19 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,8 +31,8 @@ function createGrid(gridSize) {
     for (let rowCol = 0; rowCol < gridSize ** 2; rowCol++) {
       let gridCell = document.createElement("div");
       Object.assign(gridCell.style, {
-        height : `${(size / gridSize)}px`,
-        width : `${(size / gridSize)}px`
+        height : `${(size / gridSize) - 2}px`,
+        width : `${(size / gridSize) - 2}px`
       });
       gridCell.classList.add("gridBox");
       gridCell.addEventListener('mouseover', changeGridColor)
